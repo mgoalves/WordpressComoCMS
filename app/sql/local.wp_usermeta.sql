c68f2cfe90cf5df440be48d3d28a6f3eb42bd61a
Marcello Alves <marcello_doalves@hotmail.com>
2019-05-21 07:53:16 -0300

    Aula #2.4

---
+++ app/sql/local.wp_usermeta.sql
@@ -0,0 +1,24 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_usermeta` VALUES
+(1,1,"nickname","mgoalves"),
+(2,1,"first_name",""),
+(3,1,"last_name",""),
+(4,1,"description",""),
+(5,1,"rich_editing","true"),
+(6,1,"syntax_highlighting","true"),
+(7,1,"comment_shortcuts","false"),
+(8,1,"admin_color","fresh"),
+(9,1,"use_ssl","0"),
+(10,1,"show_admin_bar_front","true"),
+(11,1,"locale",""),
+(12,1,"wp_capabilities","a:1:{s:13:\"administrator\";b:1;}"),
+(13,1,"wp_user_level","10"),
+(14,1,"dismissed_wp_pointers","theme_editor_notice"),
+(15,1,"show_welcome_panel","1"),
+(16,1,"session_tokens","a:3:{s:64:\"d839159519aac6a6fc7da8da5b802bcb567ec87ec27385503c31a973f2a3a5be\";a:4:{s:10:\"expiration\";i:1558060084;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:115:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36\";s:5:\"login\";i:1557887284;}s:64:\"0286f058ce35b0ee1e2de62871716d7ee03c970a5da421c266af7e46cb1a1596\";a:4:{s:10:\"expiration\";i:1558060086;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:115:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36\";s:5:\"login\";i:1557887286;}s:64:\"81825932e5aac835517d7f699fc93346083968a3af01ab21efcc55173a4429a0\";a:4:{s:10:\"expiration\";i:1558191959;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:115:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36\";s:5:\"login\";i:1558019159;}}"),
+(17,1,"wp_dashboard_quick_press_last_post_id","4"),
+(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(19,1,"closedpostboxes_dashboard","a:1:{i:0;s:17:\"dashboard_primary\";}"),
+(20,1,"metaboxhidden_dashboard","a:0:{}");
---
+++ app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"mgoalves","$P$B2ESp.jNRD6lV2e7gNPG63pzgdO/5a.","mgoalves","marcellodoalves@hotmail.com","","2019-05-15 02:27:15","",0,"mgoalves");
